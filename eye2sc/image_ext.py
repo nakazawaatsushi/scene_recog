efa15a639630391e05d9782bad668ad167da1dac
Nakazawa Atsushi <nakazawa.atsushi@gmail.com>
2017-12-08 12:33:48 +0000

    hoge

--- eye2sc/image_ext.py
+++ eye2sc/image_ext.py
@@ -15,6 +15,7 @@ def load_imgs_asarray(paths, grayscale=False, target_size=None,
         img = load_img(path, grayscale, target_size)
         array = img_to_array(img, dim_ordering)
         arrays.append(array)
-        print('.', end='')  
+        # print('.', end='') 
+        print(path)
     
     return numpy.asarray(arrays)
