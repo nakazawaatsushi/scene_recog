a4df3b685e682ca4adb0994730d2e56ae0122b96
Nakazawa Atsushi <nakazawa.atsushi@gmail.com>
2017-12-09 21:43:53 +0000

    result nakazawa-300epoch

--- eye2sc/image_ext.py
+++ eye2sc/image_ext.py
@@ -7,8 +7,9 @@ def list_pictures_in_multidir(paths):
     for path in paths:
         fpaths += list_pictures(path)
     
-    fpaths = fpaths.sort()
-    # print(fpaths)
+    fpaths = sorted(fpaths)
+    #fpaths = fpaths.sort()
+    print(fpaths)
        
     return fpaths
 
